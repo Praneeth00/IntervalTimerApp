4744b9729a79dfd98212c7642c174787735d003f
Praneeth Reddy Nimma <nimmapraneethreddy@gmail.com>
2025-03-06 22:21:47 -0500

    Initial Commit

---
+++ Interval Timer/Interval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TimerApp.swift
+//  Interval Timer
+//
+//  Created by Praneeth Nimma on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Interval_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Interval TimerTests/Interval_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_TimerTests.swift
+//  Interval TimerTests
+//
+//  Created by Praneeth Nimma on 3/6/25.
+//
+
+import Testing
+@testable import Interval_Timer
+
+struct Interval_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
